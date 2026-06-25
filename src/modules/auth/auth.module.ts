1045cd40009427ba7d69e3036d8669e01e292932
AngelG-Gutierrez <gadea2005@hotmail.com>
2026-06-25 01:46:18 -0600

    Feature Auth updated

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -12,7 +12,7 @@ import { AuthController } from './auth.controller';
     UsersModule,
     PassportModule,
     JwtModule.register({
-      secret: process.env.JWT_SECRET ?? '',
+      secret: process.env.JWT_SECRET || 'suszWobXGGAsk0PLh7eKVKB8QLccWRNr',
       signOptions: { expiresIn: process.env.JWT_EXP as any },
     }),
   ],
