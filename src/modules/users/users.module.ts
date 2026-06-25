6166733355cbe3858644911ede00cf252df261a7
AngelG-Gutierrez <gadea2005@hotmail.com>
2026-06-25 01:49:19 -0600

    Feature Auth updated

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
-import { UsersController } from './users.controller';
 import { PrismaService } from 'src/core/databases/prisma.service';
 
 @Module({
